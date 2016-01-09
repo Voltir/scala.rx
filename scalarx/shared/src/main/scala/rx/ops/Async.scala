359ea6eb232f7e9f1e4bf052aa66af9a48dce81c
Nick <nick@stabtechs.com>
2016-01-08 17:18:41 -0800

    todos

--- scalarx/shared/src/main/scala/rx/ops/Async.scala
+++ scalarx/shared/src/main/scala/rx/ops/Async.scala
@@ -8,29 +8,23 @@ import scala.util.Failure
 
 object OPS {
 
-  implicit class FutureCombinators[T](val src: Future[T]) extends AnyVal {
-
+  implicit class FutureCombinators[T](val f: Future[T]) extends AnyVal {
     def toRx(initial: T)(implicit ec: ExecutionContext, ctx: RxCtx): Rx[T] = {
-      @volatile var completed: T = initial
+      var completed: T = initial
       val ret = Rx.build { inner => completed }(ctx)
-      src.map { done =>
-        completed = done
-        ret.recalc()
-      }
+      f.map { v => completed = v ; ret.recalc() }
       ret
     }
-
   }
 
-  implicit class AsyncCombinators[T](val src: rx.Node[T]) extends AnyVal {
-
+  implicit class AsyncCombinators[T](val n: rx.Node[T]) extends AnyVal {
     def debounce(interval: FiniteDuration)(implicit scheduler: Scheduler, ctx: RxCtx): Rx[T] = {
-      @volatile var npt = Deadline.now
+      var npt = Deadline.now
       lazy val ret: Rx[T] = Rx.build { inner =>
-        src.Internal.addDownstream(inner)
+        n.Internal.addDownstream(inner)
         if(Deadline.now >= npt) {
           npt = Deadline.now + interval
-          src.now
+          n.now
         } else {
           scheduler.scheduleOnce(npt - Deadline.now) {
             ret.propagate()
@@ -40,6 +34,15 @@ object OPS {
       }(ctx)
       ret
     }
+
+    def delay(duration: FiniteDuration)(implicit scheduler: Scheduler, ctx: RxCtx): Rx[T] = ???
+
+    //https://github.com/MetaStack-pl/MetaRx/commit/4cc7930e7b0c9b423d33a94868d19be5cf6f1a58
+    def throttle(interval: FiniteDuration)(implicit scheduler: Scheduler, ctx: RxCtx): Rx[T] = ???
+  }
+
+  object Timer {
+    def apply(interval: FiniteDuration, delay: FiniteDuration): Rx[Long] = ???
   }
 
 }
